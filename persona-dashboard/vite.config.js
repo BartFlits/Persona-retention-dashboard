691ac53f1383dc866f163a5eef307dd9ba5d0ecf
BartFlits <152269887+BartFlits@users.noreply.github.com>
2026-01-06 10:29:27 +0100

    Update vite.config.js

--- persona-dashboard/vite.config.js
+++ persona-dashboard/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/Persona-retention-dashboard/",
+  base: "/Persona-retention-dashboard/", // push dit 
   plugins: [react()],
   resolve: {
     alias: {
